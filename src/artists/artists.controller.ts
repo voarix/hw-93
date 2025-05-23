837ad97ca3dd308744f3f3df3cf35a7a93b1b70a
Kachkynov Alikhan <t8t228@gmail.com>
2025-05-23 14:19:56 +0600

    install multer, create artist (feature) and implement CRD operations

---
+++ src/artists/artists.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Artist, ArtistDocument } from '../schemas/artist.schema';
+import { CreateArtistDto } from './create.artist.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('artists')
+export class ArtistsController {
+  constructor(
+    @InjectModel(Artist.name) private artistModel: Model<ArtistDocument>,
+  ) {}
+
+  @Get()
+  async getAll() {
+    return this.artistModel.find();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.artistModel.find({ _id: id });
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/uploads/artists' }),
+  )
+  create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() createArtistDto: CreateArtistDto,
+  ) {
+    const artist = new this.artistModel({
+      name: createArtistDto.name,
+      info: createArtistDto.info,
+      image: file ? '/uploads/artists/' + file.filename : null,
+    });
+    return artist.save();
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    await this.artistModel.findByIdAndDelete(id);
+    return { message: 'Artist was deleted successfully' };
+  }
+}
